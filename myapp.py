4c279b187dffe91ab872705e2cbffa3cf0c934d8
Kumar Vaibhav Ugratara <124518932+VaibhavUgratara@users.noreply.github.com>
2024-04-29 20:01:22 +0530

    Add files via upload

--- myapp.py
+++ myapp.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template,request,session,redirect
 from flask_sqlalchemy import SQLAlchemy
 app=Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///../bbau.db"
+app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///bbau.db"
 app.config['SQLAlCHEMY_TRACK_MODIFICATIONS']=False
 db=SQLAlchemy(app)
 
